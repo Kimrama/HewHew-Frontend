1839b6eadc3afb35eacf74a161b15ab2c925255b
Kimrama <kimrama0x@gmail.com>
2025-11-03 12:29:06 +0700

    feat: add review functionality and improve layout in delivery and order detail pages

--- app/(pages)/myDeliveryDetail.tsx
+++ app/(pages)/myDeliveryDetail.tsx
@@ -3,24 +3,27 @@ import { OrderBlock } from "@/components/OrderBlock";
 import { ThemedButton } from "@/components/ThemedButton";
 import { ThemedText } from "@/components/ThemedText";
 import { Colors } from "@/constants/Colors";
-import { useOrderContext } from "@/store/order-context";
 import { LinearGradient } from "expo-linear-gradient";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { useEffect, useState } from "react";
-import {
-  Alert,
-  Pressable,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function deliveryDetail() {
   const { orderId } = useLocalSearchParams<{ orderId: string }>();
-  const [order, setOrder] = useState<(Order & { User?: User; menus: { name: string; detail: string; price: number; quantity: number }[]; totalQuantity: number; dropOffLocation?: DropOff;})>();
+  const [order, setOrder] = useState<
+    Order & {
+      User?: User;
+      menus: {
+        name: string;
+        detail: string;
+        price: number;
+        quantity: number;
+      }[];
+      totalQuantity: number;
+      dropOffLocation?: DropOff;
+    }
+  >();
   const [loading, setLoading] = useState(true);
   const router = useRouter();
   const insets = useSafeAreaInsets();
@@ -43,7 +46,6 @@ export default function deliveryDetail() {
 
     fetchOrder();
   }, [orderId]);
-
 
   if (loading) {
     return (
@@ -95,16 +97,24 @@ export default function deliveryDetail() {
           canteen={order.canteen_name}
           menus={order.menus}
           orderPrice={order.amount}
-          name={order.User?.username ?? 'Unknown'}
-          address={order.dropOffLocation?.name ?? ''}
-          addressDetail={order.dropOffLocation?.detail ?? ''}
+          name={order.User?.username ?? "Unknown"}
+          address={order.dropOffLocation?.name ?? ""}
+          addressDetail={order.dropOffLocation?.detail ?? ""}
           deliveryMethod={order.delivery_method}
           appointmentTime={order.appointment_time}
           riderEarn={order.shipping_fee}
           type="myDeliveryDetail"
           confirmImage={order.confirmation_image_url}
         />
-
+        <ThemedButton
+          style={styles.button}
+          title={"Review Client"}
+          onPress={() =>
+            router.push(
+              `/review?orderId=${order.order_id}&targetUserId=${order.user_order_id}`
+            )
+          }
+        />
       </SafeAreaView>
     </LinearGradient>
   );
